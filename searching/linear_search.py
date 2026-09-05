b941cdda81ff867263245bc6caf6adaef6b62847
Priti Maurya <maurya.priti032001@gmail.com>
2026-09-05 11:21:33 +0530

    Refactor linear_search function and add examples
    Reformatted the linear_search function and added commented-out example code.

--- searching/linear_search.py
+++ searching/linear_search.py
@@ -9,3 +9,13 @@ arr=[10,2,6,18,100,20]
 print(linear_search(arr,6))
 
 
+#linear search
+#nums=[1,2,3,4,5,6,7,8,9]
+#target=90
+#for i,n in enumerate(nums):
+#  if n==target:
+#    print(i)
+#    break
+#else:
+#  print(-1)
+
